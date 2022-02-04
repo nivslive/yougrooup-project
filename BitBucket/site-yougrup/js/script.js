bbe0fc642adf904541c3756fd5916a7b52af5f4f
nivslive <nivanjr123@gmail.com>
2022-02-04 09:18:45 -0300

    first commit

---
+++ BitBucket/site-yougrup/js/script.js
@@ -0,0 +1,17 @@
+/** alguns tests que podem ser uteis  
+
+console.log('oi')
+
+
+let open = document.getElementById('openModal');
+let close = document.getElementById('close');
+let fade = document.getElementById('fade');
+let cntModal = document.getElementById('ctnModal');
+
+open.onclick = function() {fade.style.display = "flex }
+
+close.onclick = function() {fade.style.display = "none"}
+
+fade.onclick = function() {fade.style.display = "none"}
+
+cntModal.onclick = function(event) {event.stopPropagation()}
